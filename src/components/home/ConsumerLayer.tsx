d098f75b26f24ef9f021766f3fe67c87fd76d3a2
Kishor <yoyokunal156@gmail.com>
2023-07-09 18:09:36 +0530

    consumerlayer section added

--- src/components/home/ConsumerLayer.tsx
+++ src/components/home/ConsumerLayer.tsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import {
+  Box,
+  Center,
+  Container,
+  Flex,
+  Image,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 
-function ConsumerLayer() {
+const Section = () => {
   return (
-	<div>ConsumerLayer</div>
-  )
-}
+    <Container py="50px" maxW={1300}>
+      <Flex alignItems="center">
+        <Box flex="1" textColor="white">
+          <Text
+            fontSize="24px"
+            fontWeight="400"
+            textTransform={"uppercase"}
+            color="#DF5"
+            lineHeight={"normal"}
+            mb="24px"
+          >
+            Consumer Layer
+          </Text>
+          <Text
+            fontSize="50px"
+            fontWeight="600"
+            mb="50px"
+            lineHeight="normal"
+            width="85%"
+          >
+            “We are shaping the consumer space by combining potential of{" "}
+            <b style={{ color: "#df5" }}>Web3 and AI.”</b>
+          </Text>
+          <Text
+            fontSize={24}
+            fontWeight={400}
+            width="50%"
+            paddingLeft={1}
+            lineHeight="normal"
+          >
+            <b style={{ color: "#df5" }}>User adoption</b> is the catalyst for
+            the flourishing, democratising, and paradigm-shifting potential of
+            crypto and Web3. We build Web3's transformative with AI capabilities
+            to deliver desirable crypto ventures.
+          </Text>
+        </Box>
+        <Box display="flex" justifyItems="center">
+          <Image
+            src="/web3-banner.png"
+            alt="web3-banner"
+            width="343px"
+            height="364px"
+          />
+        </Box>
+      </Flex>
+    </Container>
+  );
+};
 
-export default ConsumerLayer
+export default Section;
