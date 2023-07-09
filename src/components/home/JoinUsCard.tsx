6819e1ae5c705e1a6b6f8c28df52f9610fc5bf5f
Kishor <yoyokunal156@gmail.com>
2023-07-09 16:50:16 +0530

    Joinuscard

--- src/components/home/JoinUsCard.tsx
+++ src/components/home/JoinUsCard.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import { Box, Button, Container, Heading, Text } from "@chakra-ui/react";
 
-function JoinUsCard() {
+const Card = () => {
   return (
-	<div>JoinUsCard</div>
-  )
-}
+    <Container maxW={1300}>
+      <Box
+        alignContent="center"
+        width={"100%"}
+        bg="white"
+        borderWidth="1px"
+        borderRadius="40"
+        overflow="hidden"
+        p={"49px 113px"}
+        display="flex"
+        alignItems="center"
+      >
+        <Box flex="1" pr={4} bg="white">
+          <Heading
+            as="h2"
+            size="lg"
+            mb={2}
+            bg="white"
+            fontWeight={600}
+            fontSize={50}
+          >
+            Build <br />
+            With Us.
+          </Heading>
+          <Text bg="white" fontWeight={400} fontSize={24} color="#414141">
+            Join us as a partner, builder or community in our <br />
+            creative journey.
+          </Text>
+        </Box>
+        <Button
+          p={"12px 40px 12px 40px"}
+          fontSize={18}
+          fontWeight={400}
+          background={"#DDFF55"}
+          size={"md"}
+          rounded={"full"}
+        >
+          Join us now
+        </Button>
+      </Box>
+    </Container>
+  );
+};
 
-export default JoinUsCard
+export default Card;
