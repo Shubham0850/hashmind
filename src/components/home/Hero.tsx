125453ff2e56cec7c81b70275f0d1563b6134434
Jayjeet1013 <jayjeet1013@gmail.com>
2023-07-09 18:12:14 +0530

    hero section added

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import { Box, Button, Container, Text } from "@chakra-ui/react";
 
 function Hero() {
   return (
-	<div>Hero</div>
-  )
+    <Container maxW={1300}>
+       <Box
+      backgroundImage="url('hero-bg.png')"
+      backgroundSize="cover"
+      border-radius= '52.462px'
+      height="622px"
+      width='1301px'
+      display="flex"
+      alignItems="center"
+      paddingLeft={'80px'}
+    >
+      <Box marginLeft={8} bg="rgba(0, 0, 0, 0)"  >
+        
+        <Text fontWeight={500} bg="rgba(0, 0, 0, 0)"  style={{fontSize:'60px',width:'626px', }} color="#DDFF55">
+        Convergence of Web3 & AI Venture Studio
+        
+        </Text>
+       
+   
+        <Box  bg="rgba(0, 0, 0, 0)" display={'flex'} py={10}>
+      <Text fontSize="40px" bg="rgba(0, 0, 0, 0)" fontWeight="600" color="white" p={4}>
+      Let’s
+        </Text>
+        <Text fontSize="60px" bg="rgba(0, 0, 0, 0)"  fontWeight="600" color="#DDFF55" >
+        Envision.
+        </Text>
+
+      </Box>
+      
+      <Button colorScheme="yellow"  size="lg">
+          Build With Us
+        </Button>
+
+      </Box> 
+    
+      
+       
+    </Box>
+    </Container>
+   
+  );
 }
 
-export default Hero
+export default Hero;
